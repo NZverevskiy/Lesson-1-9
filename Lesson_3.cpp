3701bd763dd51e84e7a61ad3a96ceff11da0c2eb
NZverevskiy <105425802+NZverevskiy@users.noreply.github.com>
2022-05-12 17:43:18 +0300

    For, While и Do While. Operators continue/break

---
+++ Lesson_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Цикл For
+    for (int i = 0; i < 10; i++)
+        cout << i << endl;
+
+    //Цикл While
+    int i = 1; // Создание переменной
+    while (i <= 10) { // Здесь только условие
+        cout << i << endl;
+        i++; // Увеличение переменной
+    }
+
+    //Цикл Do While
+    int x = 13;
+    do {
+        x--;
+        cout << i << endl;
+    } while (x > 10);
+
+    //Операторы в циклах
+    for (int b = 1; b < 10; b++) {
+        if (b % 20 == 0)
+            continue;//если условие выполнено, то эта иттерация пропускается
+        cout << "Число равно: " << b << endl;
+    }
+    for (int b = 1; b < 10; b++) {
+        if (b % 20 == 0)
+            break;//если условие выполнено, то цикл полностью завершаетсяы
+    }
+}
