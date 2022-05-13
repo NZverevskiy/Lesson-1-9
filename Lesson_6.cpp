7e215a85d6a8e19ca511947448e2cef332d67700
NZverevskiy <105425802+NZverevskiy@users.noreply.github.com>
2022-05-13 15:17:01 +0300

    Functions with array

---
+++ Lesson_6.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+float get_avarage(int *arr, int size);//Скелет функции
+float get_avarage(vector<int>* arr);//Скелет функции с массивом вектор
+//Работа с одномерным массивом
+int main()
+{
+	vector <int> prices = { 4,1,4,6,666,124,435 };
+	int balance[] = { 5,23,100,99,1,0 };
+	float avarage = get_avarage(balance, 6);
+	cout << get_avarage(&prices);
+	cout << avarage;
+	return 0;
+	}
+//функция, принимающая одномерный массив в значение 
+float get_avarage(int *arr, int size) {
+	int i, sum = 0;
+	float avg;
+	for (i = 0; i < size; i++)
+		sum += arr[i];
+	avg = float(sum) / size;
+
+	return avg;
+}
+//функция, принимающая вектор массив в значение 
+float get_avarage(vector<int>*arr) {
+	int sum = 0;
+	float avg;
+	for (float number:*arr)
+		sum +=  number;
+	avg = float(sum) / (*arr).size();
+
+	return avg;
+}
