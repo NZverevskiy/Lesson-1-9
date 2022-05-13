789c41e54225aaea9d63691b8b2d2b004e2972cd
NZverevskiy <105425802+NZverevskiy@users.noreply.github.com>
2022-05-13 17:15:45 +0300

    Enum

---
+++ Lesson_8.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+enum cars {//создание базы машин
+VOLVO,
+BMW,
+MARCEDES,
+MAZDA,
+FORD,
+BUGATTI
+};
+struct User {
+	string car;
+};
+int main()
+{
+	User admin;
+	admin.car = cars::BMW;//выбор под пользователя, добавление его в структуру
+	cout << admin.car;
+}
